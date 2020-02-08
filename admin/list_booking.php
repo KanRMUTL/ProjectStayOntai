2970ef7e41be33eedd2e49ebed907e8abc1a1d3e
kanRMUTL <kan4227@gmail.com>
2020-02-08 14:35:10 +0700

    Table responsive in List booking

--- admin/list_booking.php
+++ admin/list_booking.php
@@ -28,8 +28,8 @@
                 $sql = "SELECT * FROM tb_booking a INNER JOIN tb_booking_detail b ON a.booking_id = b.booking_id INNER JOIN tb_room d ON b.room_id = d.room_id INNER JOIN tb_homestay e ON d.homestay_id = e.homestay_id INNER JOIN tb_user g ON a.user_id = g.user_id ORDER BY a.booking_id DESC ";
                 $result = result_array($sql);
                 ?>
-
-                <div class="tb_all">
+                <!-- PC Screen -->          
+                <div class="tb_all d-none d-sm-block">
                     <table class="table table-striped table-bordered table-hover" id="table-js">
                         <thead>
                         <tr>
@@ -76,6 +76,61 @@
                         </tbody>
                     </table>
                 </div>
+                <!-- End PC Screen -->    
+
+                <!-- Mobile Screen -->          
+                <div class="tb_all d-block d-sm-none">
+                    <table class="table table-striped table-bordered table-hover" id="table-mobile">
+                        <thead>
+                        <tr>
+                            <th>รายการ</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?PHP foreach ($result as $key => $row) { ?>
+                            <tr>
+                                <td class="center">
+                                   
+                                    <ul class="list-group">
+                                        <li class="list-group-item">
+                                            <b>
+                                                ลำดับที่  <?= $key + 1; ?></li>
+                                            </b>
+                                        <li class="list-group-item text-left">
+                                            <b>เลขที่การจอง :</b> <?= booking_id($row['booking_id']); ?> <br>
+                                            <b>โฮมสเตย์ :</b> <?= $row['homestay_name'] ?> <br>
+                                            <b>ห้อง :</b> <?= $row['room_name'] ?> <br>
+                                            <?PHP
+                                            $sum = 0;
+
+                                            $sum = $row['booking_detail_price'] * ($row['booking_detail_adult'] + $row['booking_detail_child']) * $row['booking_detail_total'];
+                                            ?>
+                                            <b>ราคารวม :</b> <?= number_format($sum, 2); ?> <br>
+                                        </li>
+                                        <li class="list-group-item text-left">
+                                            <b>ผู้จอง :</b> <?= $row['user_titlename'] ?><?= $row['user_name'] ?> <?= $row['user_lastname'] ?> <br>
+                                            <b>เบอร์โทร :</b> <?= $row['user_tel']; ?> <br>
+                                            <b>อีเมล์ :</b> <?= $row['user_email']; ?> <br>
+                                            <b>ว/ด/ป :</b> <?= $row['user_birth']; ?> <br>
+                                        </li>
+                                        <li class="list-group-item text-left">
+                                            <b>สถานะ : </b><?= booking_detail_status($row['booking_detail_status']); ?>
+                                        </li>
+                                        <li class="list-group-item">
+                                        <a target="_blank" href="../print_booking.php?id=<?= $row['booking_id']; ?>" class="btn btn-info btn-rounded">
+                                            <i class="fa fa-eye"></i>
+                                        </a>
+                                        </li>
+                                    </ul>
+                                </td>
+                            </tr>
+                        <?PHP } ?>
+                        </tbody>
+                    </table>
+                </div>
+                <!-- End Mobile Screen -->          
+
+
             </div>
         </div>
     </div>
